91bb1123a2cadcd5f13eeac9edfb79c971853882
Ced2911 <Ced2911@gmail.com>
2023-06-09 09:14:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fenrir theme builder
+
+## Limitations
+- Backgrounds MUST use image with 512x512 pixel and less than 16 colors
+- Font are currently hard coded
---
+++ src/pages/screens/GameList.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import FenrirBackground from '@/components/FenrirBackground.vue'
+import FenrirConfig from '@/components/FenrirConfig.vue'
+
+export default {
+  components: {
+    FenrirConfig,
+    FenrirBackground
+  },
+  setup() {
+    // expose to template and other options API hooks
+    return {}
+  },
+
+  data() {
+    return {
+      
+    }
+  },
+  mounted() {}
+}
+</script>
+
+<template>
+ <fenrir-config />
+</template>
